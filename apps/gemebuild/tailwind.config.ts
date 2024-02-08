500707bbe15bb5f30b49386424f92d64bcdb2d8e
alex <qiushi1511+geme@gmail.com>
2024-02-08 09:52:41 +0800

    feat(gemebuild): setup geme.bio home hero section as demo

--- apps/gemebuild/tailwind.config.ts
+++ apps/gemebuild/tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from 'tailwindcss'
+import { tailwindPreset } from 'tailwind-config'
 
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -16,5 +18,6 @@ const config: Config = {
     },
   },
   plugins: [],
+  presets: [tailwindPreset],
 }
 export default config
