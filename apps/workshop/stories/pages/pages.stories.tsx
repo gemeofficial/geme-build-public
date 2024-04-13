be7a79d43fdb01b1572a299b7091155b96f453c3
alex <qiushi1511+geme@gmail.com>
2024-04-13 10:14:39 +0800

    chore(workshop): clean up pages stories by using path as story name

--- apps/workshop/stories/pages/pages.stories.tsx
+++ apps/workshop/stories/pages/pages.stories.tsx
@@ -10,20 +10,20 @@ const meta: Meta = {
 
 export default meta
 
-export const HomeV2311Story: StoryObj<typeof HomeV2311> = {
+export const HomeStory: StoryObj<typeof HomeV2311> = {
   render: () => <HomeV2311 />,
-  name: 'home-v2311',
+  name: '/',
   args: {},
 }
 
-export const PdpIntroV2311Story: StoryObj<typeof PdpIntroV2311> = {
+export const PdpIntroStory: StoryObj<typeof PdpIntroV2311> = {
   render: () => <PdpIntroV2311 />,
-  name: 'pdp-intro-v2311',
+  name: '/product/geme#intro',
   args: {},
 }
 
-export const PdpUserStoryV2311Story: StoryObj<typeof PdpUserStoryV2311> = {
+export const PdpUserStoryStory: StoryObj<typeof PdpUserStoryV2311> = {
   render: () => <PdpUserStoryV2311 />,
-  name: 'pdp-user-story-v2311',
+  name: '/product/geme#user-story',
   args: {},
 }
