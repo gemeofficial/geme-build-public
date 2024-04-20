613079270f7296159bceb942de1eb1c876add610
alex <qiushi1511+geme@gmail.com>
2024-04-20 09:45:09 +0800

    chore(workshop): dirty way to switch from vite to next.js for workshop app
    by
    
    - copying gemebuild source code
    - keep workshop package.json / stories / eslint config

--- apps/workshop/.storybook/main.ts
+++ apps/workshop/.storybook/main.ts
@@ -1,44 +1,36 @@
-import { dirname, join, resolve } from 'path'
+import type { StorybookConfig } from '@storybook/nextjs'
 
-function getAbsolutePath(value) {
+import { join, dirname } from 'path'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')))
 }
-
-const config = {
-  stories: ['../stories/*.stories.tsx', '../stories/**/*.stories.tsx'],
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     getAbsolutePath('@storybook/addon-links'),
     getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-docs'),
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: getAbsolutePath('@storybook/nextjs'),
     options: {},
   },
-
-  core: {},
-
-  async viteFinal(config, { configType }) {
-    // customize the Vite config here
-    return {
-      ...config,
-      define: { 'process.env': {} },
-      resolve: {
-        alias: [
-          {
-            find: 'ui',
-            replacement: resolve(__dirname, '../../../packages/ui/'),
-          },
-        ],
-      },
-    }
-  },
-
   docs: {
     autodocs: 'tag',
   },
-
   staticDirs: ['../../gemebuild/public'], //👈 Configures the static asset folder in Storybook
+  webpackFinal: async (config) => {
+    if (config.resolve != null) {
+      config.resolve.plugins = [new TsconfigPathsPlugin()]
+    }
+    return config
+  },
 }
-
 export default config
