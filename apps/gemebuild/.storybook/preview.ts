38ae9831cd2c7b70ef4073d4659032160a27ad2b
alex <qiushi1511+geme@gmail.com>
2024-05-06 19:51:04 +0800

    chore(deps): upgrade storybook for gemebuild

--- apps/gemebuild/.storybook/preview.ts
+++ apps/gemebuild/.storybook/preview.ts
@@ -3,7 +3,8 @@ import type { Preview } from '@storybook/react'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    // Cf: https://storybook.js.org/docs/essentials/actions#via-storybooktest-fn-spy-function
+    // actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
