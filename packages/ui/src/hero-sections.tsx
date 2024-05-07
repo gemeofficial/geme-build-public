92accd002b45682af31512688cbd20d32d27a2f9
yanyusheng <higeme530@gmail.com>
2024-05-07 18:11:54 +0800

    修改小窗口大小

--- packages/ui/src/hero-sections.tsx
+++ packages/ui/src/hero-sections.tsx
@@ -139,7 +139,7 @@ function HeroSection1({
       >
         <div className="portrait:w-48 portrait:h-28 absolute right-[18px] bottom-[18px] z-40">
           {/* 右下角视频小窗 */}
-          <div className="relative h-full landscape:w-[180px] landscape:h-[111px] landscape:lg:w-[230px] landscape:lg:h-[132px] landscape:2xl:w-[270px] landscape:2xl:h-[152px] rounded-full landscape:rounded-xl overflow-hidden group">
+          <div className="relative h-full landscape:w-[180px] landscape:h-[111px] landscape:lg:w-[270px] landscape:lg:h-[152px]  landscape:2xl:w-[400px] landscape:2xl:h-[230px] rounded-xl landscape:rounded-xl overflow-hidden group">
             <div className="relative h-full w-full object-cover transform-gpu transition-transform group-hover:scale-125 duration-[0.25s] ease-[cubic-bezier(0.24, 0.8, 0.4, 1)]">
               <MuxVideoPlayer {...videoProps} />
             </div>
