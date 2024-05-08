ae743c702dc0f6cfdbe4b60c8bf61c58a7f6e33f
yanyusheng <higeme530@gmail.com>
2024-05-08 13:14:03 +0800

    修复在bio中传入自定义的Prefetch Link 时多语言路径不正确问题

--- packages/ui/src/client-components/HeroSectionsButton.tsx
+++ packages/ui/src/client-components/HeroSectionsButton.tsx
@@ -20,6 +20,13 @@ export default function HeroSectionsButton({
       From: 'Hero Primary Button',
     })
   }
+
+  // 修复在bio中传入自定义的Prefetch Link 时多语言路径不正确问题
+  let newLinkUrl = ''
+  if (LinkComponent) {
+    newLinkUrl = linkUrl?.replace('/de','').replace('/fr','') as string
+  }
+
   return (
     <>
       {linkText && linkUrl && LinkComponent == null && (
@@ -36,7 +43,7 @@ export default function HeroSectionsButton({
         <span onClick={linkClickedTrack}>
           <LinkComponent
             className=" v2311-font-h3 font-bold text-white xl:w-auto flex items-center justify-center mt-[80px] md:mt-[160px] xl:mt-16 border-solid border-2 border-white rounded-xl py-1 md:py-2 px-16 md:px-32 xl:px-8"
-            href={linkUrl}
+            href={newLinkUrl}
           >
             {linkText}
             <IconCycleArrow className="inline-block ml-4 md:ml-8 xl:ml-4 w-5 md:w-10 xl:w-12 h-5 md:h-10 xl:h-12" />
