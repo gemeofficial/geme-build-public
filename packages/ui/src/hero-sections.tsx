ca401b05b5c6aea05953fab1f47d12a33ba7b00b
yanyusheng <higeme530@gmail.com>
2024-05-13 16:01:54 +0800

    整理样式

--- packages/ui/src/hero-sections.tsx
+++ packages/ui/src/hero-sections.tsx
@@ -110,7 +110,16 @@ function HeroSection1({
           {/* 右下角视频小窗 */}
           <div className="relative h-full landscape:w-[180px] landscape:h-[111px] landscape:lg:w-[270px] landscape:lg:h-[152px]  landscape:2xl:w-[400px] landscape:2xl:h-[230px] rounded-xl landscape:rounded-xl overflow-hidden group">
             <div className="relative h-full w-full object-cover transform-gpu transition-transform group-hover:scale-125 duration-[0.25s] ease-[cubic-bezier(0.24, 0.8, 0.4, 1)]">
-              <video autoPlay playsInline muted loop poster={videoProps.posterUrl} src={videoProps.src}>
+              <video
+                className="w-full h-full object-cover"
+                controls={false}
+                autoPlay
+                playsInline
+                muted
+                loop
+                poster={videoProps.posterUrl}
+                src={videoProps.src}
+              >
                 Your browser does not support HTML5 video.
               </video>
             </div>
