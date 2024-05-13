4dec6bdc345b1e2e5399693664ee3a69ba06b7a6
yanyusheng <higeme530@gmail.com>
2024-05-13 15:24:47 +0800

    将小视频播放器换为原生video

--- packages/ui/src/hero-sections.tsx
+++ packages/ui/src/hero-sections.tsx
@@ -12,7 +12,10 @@ interface IHeroSection1Props {
   description?: ReactNode
   linkText?: string
   linkUrl?: string
-  videoProps?: IMuxVideoPlayerProps
+  videoProps: {
+    src: string // 视频源的URL
+    posterUrl: string // 视频源封面
+  }
   LinkComponent?: ILinkComponent
   fullScreenVideoUrl?: string
 }
@@ -43,22 +46,8 @@ const heroSection1Props: IHeroSection1Props = {
   linkText: 'Shop Now',
   linkUrl: '/product/geme',
   videoProps: {
-    sources: [
-      // for pc
-      {
-        minWidth: 1280,
-        src: 'https://public-assest-434759801795.s3.us-west-1.amazonaws.com/geme-bio-home-hero-backgruond-video-1080p.mp4',
-        playbackId: '4xlpgl6PcY6d8N3H5FowWDdP01kCupf3Pn8A101zaCnFo',
-        posterUrl:
-          'https://public-assest-434759801795.s3.us-west-1.amazonaws.com/cover-v2.jpg',
-      },
-      // for mobile/tablet
-      {
-        minWidth: 0,
-        src: 'https://public-assest-434759801795.s3.us-west-1.amazonaws.com/geme-bio-home-background-video-no-sound-480p.mp4',
-        playbackId: '4xlpgl6PcY6d8N3H5FowWDdP01kCupf3Pn8A101zaCnFo',
-      },
-    ],
+    src: 'https://public-assest-434759801795.s3.us-west-1.amazonaws.com/geme-bio-home-hero-backgruond-video-1080p.mp4',
+    posterUrl: '/assets/images/home-v2311/hero-blurred.webp',
   },
 }
 
@@ -121,7 +110,9 @@ function HeroSection1({
           {/* 右下角视频小窗 */}
           <div className="relative h-full landscape:w-[180px] landscape:h-[111px] landscape:lg:w-[270px] landscape:lg:h-[152px]  landscape:2xl:w-[400px] landscape:2xl:h-[230px] rounded-xl landscape:rounded-xl overflow-hidden group">
             <div className="relative h-full w-full object-cover transform-gpu transition-transform group-hover:scale-125 duration-[0.25s] ease-[cubic-bezier(0.24, 0.8, 0.4, 1)]">
-              <MuxVideoPlayer {...videoProps} />
+              <video autoPlay playsInline muted loop poster={videoProps.posterUrl} src={videoProps.src}>
+                Your browser does not support HTML5 video.
+              </video>
             </div>
 
             <button
