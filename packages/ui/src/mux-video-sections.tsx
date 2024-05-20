c0bbf30fbca07100bf22ae5f5434c22d4f7e1d2e
yanyusheng <higeme530@gmail.com>
2024-05-20 14:15:57 +0800

    完全删除mux依赖

--- packages/ui/src/mux-video-sections.tsx
+++ packages/ui/src/mux-video-sections.tsx
@@ -1,25 +1,25 @@
-'use client'
+// 'use client'
 
-import MuxPlayer from '@mux/mux-player-react'
+// import MuxPlayer from '@mux/mux-player-react'
 
-function ExampleMuxVideoPlayer() {
-  return (
-    <MuxPlayer
-      className="absolute inset-0"
-      streamType="on-demand"
-      playbackId="G2OT5ie5cwjcn6f5hCr5Sev01H4qyEmAPbUi020102XN7Lw"
-      metadata={{
-        video_id: 'video-id-54321',
-        video_title: 'Test video title',
-        viewer_user_id: 'user-id-007',
-      }}
-      autoPlay={'muted'}
-      loop
-      style={{ '--media-object-fit': 'cover', '--controls': 'none' } as any}
-      accentColor="#00945E"
-      thumbnailTime={0}
-    />
-  )
-}
+// function ExampleMuxVideoPlayer() {
+//   return (
+//     <MuxPlayer
+//       className="absolute inset-0"
+//       streamType="on-demand"
+//       playbackId="G2OT5ie5cwjcn6f5hCr5Sev01H4qyEmAPbUi020102XN7Lw"
+//       metadata={{
+//         video_id: 'video-id-54321',
+//         video_title: 'Test video title',
+//         viewer_user_id: 'user-id-007',
+//       }}
+//       autoPlay={'muted'}
+//       loop
+//       style={{ '--media-object-fit': 'cover', '--controls': 'none' } as any}
+//       accentColor="#00945E"
+//       thumbnailTime={0}
+//     />
+//   )
+// }
 
-export { ExampleMuxVideoPlayer }
+// export { ExampleMuxVideoPlayer }
--- packages/ui/src/mux-video.tsx
+++ packages/ui/src/mux-video.tsx
@@ -1,99 +1,99 @@
-'use client'
+// 'use client'
 
-import MuxPlayer from '@mux/mux-player-react'
-import { FC, useState, useEffect, VideoHTMLAttributes } from 'react'
-import Image from 'next/image'
+// import MuxPlayer from '@mux/mux-player-react'
+// import { FC, useState, useEffect, VideoHTMLAttributes } from 'react'
+// import Image from 'next/image'
 
-interface IMuxVideoSource {
-  minWidth: number // 屏幕最小宽度
-  src: string // 视频源的URL
-  playbackId: string // 视频源的Mux ID
-  posterUrl?: string // 视频源封面
-}
+// interface IMuxVideoSource {
+//   minWidth: number // 屏幕最小宽度
+//   src: string // 视频源的URL
+//   playbackId: string // 视频源的Mux ID
+//   posterUrl?: string // 视频源封面
+// }
 
-interface IMuxVideoPlayerProps extends VideoHTMLAttributes<HTMLVideoElement> {
-  sources?: IMuxVideoSource[] // 接受一组视频源
-}
+// interface IMuxVideoPlayerProps extends VideoHTMLAttributes<HTMLVideoElement> {
+//   sources?: IMuxVideoSource[] // 接受一组视频源
+// }
 
-const MuxVideoPlayer: FC<IMuxVideoPlayerProps> = ({
-  sources = [],
-  ...props
-}) => {
-  const [isCanPlay, setIsCanPlay] = useState(false)
-  const [currentPlaybackId, setCurrentPlaybackId] = useState<string>('')
-  const [currentPosterUrl, setCurrentPosterUrl] = useState<string>(
-    sources?.[0]?.posterUrl || '',
-  )
+// const MuxVideoPlayer: FC<IMuxVideoPlayerProps> = ({
+//   sources = [],
+//   ...props
+// }) => {
+//   const [isCanPlay, setIsCanPlay] = useState(false)
+//   const [currentPlaybackId, setCurrentPlaybackId] = useState<string>('')
+//   const [currentPosterUrl, setCurrentPosterUrl] = useState<string>(
+//     sources?.[0]?.posterUrl || '',
+//   )
 
-  useEffect(() => {
-    const updateSource = () => {
-      // 根据屏幕宽度找到合适的视频源
-      const suitableSource = sources
-        .slice()
-        .sort((a, b) => {
-          if (a.minWidth < b.minWidth) {
-            return -1
-          }
-          if (a.minWidth > b.minWidth) {
-            return 1
-          }
-          return 0
-        })
-        .reverse() // 从最大宽度开始检查
-        .find((source) => window.innerWidth >= source.minWidth)
+//   useEffect(() => {
+//     const updateSource = () => {
+//       // 根据屏幕宽度找到合适的视频源
+//       const suitableSource = sources
+//         .slice()
+//         .sort((a, b) => {
+//           if (a.minWidth < b.minWidth) {
+//             return -1
+//           }
+//           if (a.minWidth > b.minWidth) {
+//             return 1
+//           }
+//           return 0
+//         })
+//         .reverse() // 从最大宽度开始检查
+//         .find((source) => window.innerWidth >= source.minWidth)
 
-      if (suitableSource) {
-        setCurrentPlaybackId(suitableSource.playbackId)
-        if (suitableSource.posterUrl) {
-          setCurrentPosterUrl(suitableSource.posterUrl)
-        }
-      }
-    }
+//       if (suitableSource) {
+//         setCurrentPlaybackId(suitableSource.playbackId)
+//         if (suitableSource.posterUrl) {
+//           setCurrentPosterUrl(suitableSource.posterUrl)
+//         }
+//       }
+//     }
 
-    updateSource()
-    window.addEventListener('resize', updateSource)
+//     updateSource()
+//     window.addEventListener('resize', updateSource)
 
-    // 清理函数
-    return () => {
-      window.removeEventListener('resize', updateSource)
-    }
-  }, [sources]) // 依赖项为sources数组
+//     // 清理函数
+//     return () => {
+//       window.removeEventListener('resize', updateSource)
+//     }
+//   }, [sources]) // 依赖项为sources数组
 
-  return (
-    <>
-      <Image
-        src={currentPosterUrl}
-        alt="Video cover picture"
-        fill={true}
-        loading="eager"
-        className={`w-full h-full object-cover ${
-          isCanPlay ? 'hidden' : 'block'
-        }`}
-      />
+//   return (
+//     <>
+//       <Image
+//         src={currentPosterUrl}
+//         alt="Video cover picture"
+//         fill={true}
+//         loading="eager"
+//         className={`w-full h-full object-cover ${
+//           isCanPlay ? 'hidden' : 'block'
+//         }`}
+//       />
 
-      <MuxPlayer
-        streamType="on-demand"
-        playbackId={currentPlaybackId}
-        // metadataVideoTitle="Placeholder (optional)"
-        // metadataViewerUserId="Placeholder (optional)"
-        // primaryColor="#FFFFFF"
-        // secondaryColor="#000000"
-        autoPlay="muted"
-        className={`relative w-full h-full overflow-hidden object-cover
-        ${isCanPlay ? 'block' : 'hidden'}`}
-        style={{
-          // @ts-ignore
-          '--controls': 'none', // 隐藏控件
-          '--media-object-fit': 'cover',
-        }}
-        nohotkeys={true}
-        loop={true}
-        {...props}
-        onPlaying={() => setIsCanPlay(true)}
-      />
-    </>
-  )
-}
+//       <MuxPlayer
+//         streamType="on-demand"
+//         playbackId={currentPlaybackId}
+//         // metadataVideoTitle="Placeholder (optional)"
+//         // metadataViewerUserId="Placeholder (optional)"
+//         // primaryColor="#FFFFFF"
+//         // secondaryColor="#000000"
+//         autoPlay="muted"
+//         className={`relative w-full h-full overflow-hidden object-cover
+//         ${isCanPlay ? 'block' : 'hidden'}`}
+//         style={{
+//           // @ts-ignore
+//           '--controls': 'none', // 隐藏控件
+//           '--media-object-fit': 'cover',
+//         }}
+//         nohotkeys={true}
+//         loop={true}
+//         {...props}
+//         onPlaying={() => setIsCanPlay(true)}
+//       />
+//     </>
+//   )
+// }
 
-export { MuxVideoPlayer }
-export type { IMuxVideoPlayerProps }
+// export { MuxVideoPlayer }
+// export type { IMuxVideoPlayerProps }
