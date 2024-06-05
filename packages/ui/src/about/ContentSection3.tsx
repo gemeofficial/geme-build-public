bccebb35143f23d044e220b6bdabceb7f1d969a1
y <higeme530@gmail.com>
2024-06-06 01:36:28 +0800

    team section

--- packages/ui/src/about/ContentSection3.tsx
+++ packages/ui/src/about/ContentSection3.tsx
@@ -1,7 +1,85 @@
 import Image from 'next/image'
 
 const data = {
-  title: 'Our people',
+  title: 'Our People',
+  description:
+    ' We are a global team, with great talents come from all over the world. Combining all skill set to do one thing and do it well.',
+  people: [
+    {
+      name: 'MARCOEN Jean Marie',
+      title: 'Co-Founder / Chief Science Researcher',
+    },
+    {
+      name: 'Vivian Law',
+      title: 'Co-Founder / Chief Trouble Maker',
+    },
+    {
+      name: 'Athom Ng',
+      title: 'Co-Founder / Chief Problem Solver',
+    },
+
+    {
+      name: 'Hans King',
+      title: 'Microorganism Researcher',
+    },
+    {
+      name: 'Nobuto',
+      title: 'Microorganism Researcher',
+    },
+    {
+      name: 'Alex Yun',
+      title: 'VP, Sales',
+    },
+
+    {
+      name: 'Ethan Watson',
+      title: 'VP, Logistics',
+    },
+    {
+      name: 'Isac Chan',
+      title: 'VP, eCommerce',
+    },
+    {
+      name: 'Joey Lee',
+      title: 'VP, Customer Experience',
+    },
+    {
+      name: 'Jack Lu',
+      title: 'VP, Marketing',
+    },
+    {
+      name: 'Kacy',
+      title: 'Director, Marketing',
+    },
+    {
+      name: 'Martin Miller',
+      title: 'Director, Sales',
+    },
+    {
+      name: 'Stella Jo',
+      title: 'Chief Designer',
+    },
+    {
+      name: 'Alex Bang',
+      title: 'Platform Engineer',
+    },
+    {
+      name: 'Johnson Yan',
+      title: 'Web Engineer',
+    },
+    {
+      name: 'Yi Zhang',
+      title: 'Product Engineer',
+    },
+    {
+      name: 'Tom Puro',
+      title: 'Customer Experience',
+    },
+    {
+      name: 'Ken Stone',
+      title: 'Partner, Creative',
+    },
+  ],
   desc: 'Quasi est quaerat. Sit molestiae et. Provident ad dolorem occaecati eos iste. Soluta rerum quidem minus ut molestiae velit error quod. Excepturi quidem expedita molestias quas.',
   bodyText:
     'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat. Quasi aperiam sit non sit neque reprehenderit.',
@@ -16,6 +94,203 @@ const data = {
 }
 
 export default function ContentSection3() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-amber-400 sm:text-4xl">
+            {data.title}
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            {data.description}
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
+        >
+          {data.people.map((item, index) => (
+            <li key={index}>
+              <img
+                className="mx-auto h-24 w-24 rounded-full"
+                src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80"
+                alt=""
+              />
+              <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">
+                {item.name}
+              </h3>
+              <p className="text-sm leading-6 text-gray-600">{item.title}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-amber-400 sm:text-4xl">
+            Our People
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            We are a global team, with great talents come from all over the
+            world. Combining all skill set to do one thing and do it well.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2"
+        >
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+
+          <li>
+            <div className="flex items-center gap-x-6">
+              <img
+                className="h-16 w-16 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+              <div>
+                <h3 className="text-base font-semibold leading-7 tracking-tight text-gray-900">
+                  Leslie Alexander
+                </h3>
+                <p className="text-sm font-semibold leading-6 text-indigo-600">
+                  Co-Founder / CEO
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+
   return (
     <div className="mt-24 md:mt-40 mx-auto max-w-xl lg:max-w-7xl px-8 lg:px-12 2xl:px-0">
       <div className="lg:flex items-start justify-between">
