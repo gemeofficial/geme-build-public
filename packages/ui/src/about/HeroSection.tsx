085d2c50f0f8951338b3e9cc3b4ed9fcb7547cbc
y <higeme530@gmail.com>
2024-06-16 22:07:06 +0800

    fix build error

--- packages/ui/src/about/HeroSection.tsx
+++ packages/ui/src/about/HeroSection.tsx
@@ -3,12 +3,19 @@ import Image from 'next/image'
 const data = {
   title: 'Small Findings in the Lab, Big Impact to the World',
   // title: 'Small Microbes in the Lab, Big Impact on the World',
-  desctiptions: [
-    'At ROKH, we are united by a shared vision and a common goal: to create a world where waste is no longer a burden, but a resource, and where nature thrives alongside human progress.',
-    // 'ROKH SRL is the company behind GEME brand, we committed to providing intelligent and eco-friendly waste management solutions, harnessing the power of nature to remediate damaged environments.',
-    // 'ROKH combines the use of smart devices with natural microorganisms to blend modern technology into the natural cycle of the laws of life, reducing the damage to nature and easing the burden on the earth.',
-    // 'We discovered, developed and deliver amazing microbiota to improve and treat polluted water and soil, compensate for the damage done to nature, and heal our wounded earth.',
-    'We are driven by a passion for scientific innovation and a deep commitment to making a positive impact on the world. Through the commercialization of our groundbreaking biotechnology solutions, we aim to translate our research achievements into tangible benefits that enhance human well-being and address pressing global challenges.',
+  descriptions: [
+    <>
+      At ROKH, we are united by a shared vision and a common goal: to create a
+      world where waste is no longer a burden, but a resource, and where nature
+      thrives alongside human progress.
+    </>,
+    <>
+      We are driven by a passion for scientific innovation and a deep commitment
+      to making a positive impact on the world. Through the commercialization of
+      our groundbreaking biotechnology solutions, we aim to translate our
+      research achievements into tangible benefits that enhance human well-being
+      and address pressing global challenges.
+    </>,
     <>
       We are a team of passionate individuals
       <li>
@@ -48,11 +55,11 @@ export default function HeroSection() {
           <div className="font-bold tracking-tight text-emerald-600">
             {data.title}
           </div>
-          {data.desctiptions.map((item, index) => (
+          {data.descriptions.map((item, index) => (
             <div
               key={item}
               className={`${
-                index === data.desctiptions.length - 1 ? 'md:w-[65%]' : ''
+                index === data.descriptions.length - 1 ? 'md:w-[65%]' : ''
               } lg:w-full mt-3 md:mt-5 max-w-md text-lg text-gray-500 lg:text-xl md:max-w-3xl`}
             >
               {item}
