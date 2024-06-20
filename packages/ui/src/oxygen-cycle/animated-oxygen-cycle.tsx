ce3e0730a92cd33b77c7f70d01640d72880aaadc
yanyusheng <higeme530@gmail.com>
2024-06-20 17:07:47 +0800

    修复了默认header中的圆圈滚动位置

--- packages/ui/src/oxygen-cycle/animated-oxygen-cycle.tsx
+++ packages/ui/src/oxygen-cycle/animated-oxygen-cycle.tsx
@@ -26,9 +26,16 @@ function ScrollTriggeredAnimatedOxygenCycle(props: IOxygenCycleProps) {
         const rootHeaderHeight =
           rootHeaderEl?.getBoundingClientRect().height || 0
 
+        // 使用一个变量 适配gemebuild中无headerDOM
         const topOffset = rootHeaderHeight > 0 ? rootHeaderHeight + 100 : '12%'
 
-        // 适配部分老款分辨率低的机型
+        // pc-适配分辨率较小的笔记本电脑屏幕
+        if (window.innerWidth > 1024 && window.innerWidth <= 1800) {
+          // 80是圆圈标题的paddingTop 48是title的高度 64是圆圈的marginTop
+          return `top top-=${80 + 48 + 64}`
+        }
+
+        // Mobile-适配部分老款分辨率低的机型
         if (window.innerHeight <= 680 && typeof topOffset === 'number') {
           return `top top+=${topOffset - 60}`
         }
@@ -48,14 +55,13 @@ function ScrollTriggeredAnimatedOxygenCycle(props: IOxygenCycleProps) {
       invalidateOnRefresh={true}
       fastScrollEnd={2000}
       // markers：在调试滚动时  可打开此属性
-      // markers={{
-      //   startColor: 'green',
-      //   endColor: 'red',
-      //   fontSize: '18px',
-      //   fontWeight: 'bold',
-      //   indent: 20,
-      // }}
-      
+      markers={{
+        startColor: 'green',
+        endColor: 'red',
+        fontSize: '18px',
+        fontWeight: 'bold',
+        indent: 20,
+      }}
     >
       <Tween>
         <ForwardedRefOxygenCycle
