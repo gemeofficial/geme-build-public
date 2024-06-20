cfd07d33536aecd0267a193dd2bbdfcfaa82a932
yanyusheng <higeme530@gmail.com>
2024-06-20 18:01:09 +0800

    再次优化细节

--- packages/ui/src/oxygen-cycle/animated-oxygen-cycle.tsx
+++ packages/ui/src/oxygen-cycle/animated-oxygen-cycle.tsx
@@ -26,21 +26,37 @@ function ScrollTriggeredAnimatedOxygenCycle(props: IOxygenCycleProps) {
         const rootHeaderHeight =
           rootHeaderEl?.getBoundingClientRect().height || 0
 
-        // 使用一个变量 适配gemebuild中无headerDOM
-        const topOffset = rootHeaderHeight > 0 ? rootHeaderHeight + 100 : '12%'
+        // 80是圆圈标题的paddingTop 48是title的高度 64是圆圈的marginTop
+        const scrollStartOffsetTop = 80 - 48 - 64
+        const gemebuildScrollStartOffsetTop = 80 + 48 + 64
 
         // pc-适配分辨率较小的笔记本电脑屏幕
         if (window.innerWidth > 1024 && window.innerWidth <= 1800) {
-          // 80是圆圈标题的paddingTop 48是title的高度 64是圆圈的marginTop
-          return `top top-=${80 + 48 + 64}`
+          if (rootHeaderHeight > 0) {
+            // 如果有HeaderNotifyBar的存在的话 则需要再往上偏移80px(圈内icon的marginTop)
+            if (rootHeaderHeight > 120) {
+              return `top top-=${scrollStartOffsetTop + 80}`
+            }
+
+            // 这里无法使用scrollStartOffsetTop的原因是：例如rootHeaderHeight = 96 >> 96-(-32) = 128
+            // 但预期这里的值是一个负数，所以不能使用常量 需要直接在模板字符串中进行运算
+            return `top top+=${rootHeaderHeight - 80 - 48 - 64}`
+          }
+          // else是适配gemebuild中无header的情况的
+          else {
+            return `top top-=${gemebuildScrollStartOffsetTop}`
+          }
         }
 
         // Mobile-适配部分老款分辨率低的机型
-        if (window.innerHeight <= 680 && typeof topOffset === 'number') {
-          return `top top+=${topOffset - 60}`
+        if (window.innerHeight <= 680) {
+          if (rootHeaderHeight > 0) {
+            // 80是圆圈标题的paddingTop 48是title的高度 64是圆圈的marginTop
+            return `top top+=${rootHeaderHeight + 40}`
+          }
         }
 
-        return `top top+=${topOffset}`
+        return `top top+=12%`
       }}
       end={() => {
         // 若是在移动触控设备上（包括平板） 则将滚动结束的位置放大125vh。从而实现在移动端中的触控时更缓慢的滚动进度
@@ -55,13 +71,13 @@ function ScrollTriggeredAnimatedOxygenCycle(props: IOxygenCycleProps) {
       invalidateOnRefresh={true}
       fastScrollEnd={2000}
       // markers：在调试滚动时  可打开此属性
-      markers={{
-        startColor: 'green',
-        endColor: 'red',
-        fontSize: '18px',
-        fontWeight: 'bold',
-        indent: 20,
-      }}
+      // markers={{
+      //   startColor: 'green',
+      //   endColor: 'red',
+      //   fontSize: '18px',
+      //   fontWeight: 'bold',
+      //   indent: 20,
+      // }}
     >
       <Tween>
         <ForwardedRefOxygenCycle
