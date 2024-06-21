40fe022c08884de7403cac5de9ab1c23e30d7b75
yanyusheng <higeme530@gmail.com>
2024-06-21 14:14:15 +0800

    fix server build error

--- packages/ui/src/lib/get-dom-info.ts
+++ packages/ui/src/lib/get-dom-info.ts
@@ -1,10 +1,13 @@
-
 /**
  * 传入选择器 获取DOM与该DOM的高度 需要引用者自行控制执行时机
  * @param selector 选择器
  */
 export function getDomInfo(selector: string) {
-  const el = document.querySelector(selector)
+  if (!window || typeof window === 'undefined') {
+    return { el: null, elHeight: 0 }
+  }
+
+  const el = window.document.querySelector(selector)
   const elHeight =
     el?.getBoundingClientRect().height || 0
 
