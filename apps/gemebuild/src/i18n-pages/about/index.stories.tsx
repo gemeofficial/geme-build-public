e9cb7c01d01951809eaa2b96ad93a3af2775ba9f
yanyusheng <higeme530@gmail.com>
2024-06-21 20:01:24 +0800

    fix error

--- apps/gemebuild/src/i18n-pages/about/index.stories.tsx
+++ apps/gemebuild/src/i18n-pages/about/index.stories.tsx
@@ -4,7 +4,7 @@ import { AboutPageDe, AboutPageEn, AboutPageFr } from '.'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Pages/geme-dots-loyalty-program',
+  title: 'Pages/about',
 } satisfies Meta<typeof AboutPageEn>
 
 export default meta
