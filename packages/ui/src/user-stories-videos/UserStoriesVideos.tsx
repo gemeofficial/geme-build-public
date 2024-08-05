08fea7d8af7ee0238b5b6106c8f7adc2521f1d54
yanyusheng <higeme530@gmail.com>
2024-08-05 14:08:29 +0800

    完成基础不拒绝

---
+++ packages/ui/src/user-stories-videos/UserStoriesVideos.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import Image from 'next/image'
+import { SectionContainer, SectionTitle } from '../ui-components'
+import VideosItem, { IVideosItemProps } from './VideosItem'
+import VideoInlinePlayer from '../video-inline-player'
+
+export interface IUserStoriesVideoProps {
+  title: string
+  videos: IVideosItemProps[]
+}
+
+const data: IUserStoriesVideoProps = {
+  title: 'Real customer stories',
+  videos: [
+    {
+      type: 'mp4',
+      src: 'https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4',
+      coverImageUrl:
+        'https://a-us.storyblok.com/f/1019164/936x1248/6c44fa2f84/customer_03.png/m/780x0',
+      coverAlt: 'img',
+      buttonText: 'Jesse, Devon & Alex',
+    },
+    {
+      type: 'youtube',
+      src: 'https://www.youtube.com/embed/nlsTg4l_3zE',
+      coverImageUrl:
+        'https://a-us.storyblok.com/f/1019164/936x1248/596a0a6251/customer_02.png/m/780x0',
+      coverAlt: 'img',
+      buttonText: 'example',
+    },
+    {
+      type: 'tiktok',
+      src: 'https://www.tiktok.com/@kc_smithwrites/video/7312898892079697198?_r=1&_t=8oCUfwAy70s',
+      coverImageUrl:
+        'https://a-us.storyblok.com/f/1019164/2048x3071/3564ef7ea9/customer_01.jpeg/m/780x0',
+      coverAlt: 'img',
+      buttonText: 'example',
+    },
+  ],
+}
+
+export function UserStoriesVideos() {
+  const { title, videos } = data
+  return (
+    <SectionContainer>
+      <SectionTitle>{title}</SectionTitle>
+
+      <div className="mt-4 md:mt-8 grid grid-cols-3 gap-4 md:gap-8">
+        {videos.map((item) => (
+          <div className=" " key={item.src}>
+            {item.type === 'youtube' && (
+              <VideoInlinePlayer videoUrl={item.src} hiddenPlayIcon={true}>
+                <VideosItem item={item} />
+              </VideoInlinePlayer>
+            )}
+          </div>
+        ))}
+      </div>
+    </SectionContainer>
+  )
+}
---
+++ packages/ui/src/user-stories-videos/VideosItem.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image'
+
+type videoType = 'mp4' | 'youtube' | 'tiktok' | 'facebook' | 'reels'
+
+export interface IVideosItemProps {
+  type: videoType
+  src: string
+  coverImageUrl: string
+  coverAlt: string
+  buttonText: string
+}
+
+export default function VideosItem({ item }: { item: IVideosItemProps }) {
+  return (
+    <div className="rounded-2xl overflow-hidden cursor-pointer relative group">
+      <Image
+        src={item.coverImageUrl}
+        width={400}
+        height={550}
+        className="w-full h-full object-cover"
+        alt={item.coverAlt}
+      />
+      <div className="absolute bottom-[12%] inset-x-0 flex justify-center group-hover:scale-105 transition-all duration-300">
+        <div className="flex p-2 pr-4 items-center justify-center gap-x-2 bg-gray-200 rounded-full">
+          <button className="bg-v2311-bg-dark-green p-2 rounded-full">
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 text-white"
+            >
+              <path
+                d="M16.6742 12.8437L8.53688 18.022C7.87115 18.4456 7 17.9674 7 17.1783V6.82169C7 6.0326 7.87115 5.55439 8.53688 5.97803L16.6742 11.1564C17.2917 11.5493 17.2917 12.4507 16.6742 12.8437Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+          </button>
+          <p className="text-v2311-primary">{item.buttonText}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
