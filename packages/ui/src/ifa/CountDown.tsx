160b6e2de6c2aa6f6db01801891d3418714dea91
yanyusheng <higeme530@gmail.com>
2024-08-09 15:32:10 +0800

    优化倒计时组件

--- packages/ui/src/ifa/CountDown.tsx
+++ packages/ui/src/ifa/CountDown.tsx
@@ -2,11 +2,26 @@
 import { PropsWithChildren, useEffect, useState } from 'react'
 import Countdown, { zeroPad } from 'react-countdown'
 import { LocaleType } from '../reviews'
+import moment from 'moment'
 
 const dayText = {
   en: ['Day', 'Days'],
   de: ['Tag', 'Tage'],
   fr: ['Jour', 'Jours'],
+}
+
+function getTimeRemaining(futureTimestamp: number | Date | string) {
+  const now = moment() // 获取当前时间
+  const future = moment(futureTimestamp) // 将时间戳转换为 moment 对象
+
+  const duration = moment.duration(future.diff(now)) // 计算未来时间与当前时间的差值
+
+  const days = Math.floor(duration.asDays())
+  const hours = duration.hours()
+  const minutes = duration.minutes()
+  const seconds = duration.seconds()
+
+  return { days, hours, minutes, seconds }
 }
 
 export default function CountDown({
@@ -29,55 +44,91 @@ export default function CountDown({
   function getDayText(days: number) {
     const dayIndex = 0
     const daysIndex = 1
-    const currentIndex = days === 1 ? dayIndex : daysIndex
+    const currentIndex = days <= 1 ? dayIndex : daysIndex
     return dayText[locale][currentIndex]
   }
 
+  const staticTimeInfo = getTimeRemaining(date)
+
   return (
-    isLoaded && (
-      <Countdown
-        date={date}
-        renderer={({ days, hours, minutes, seconds, completed }) => {
-          // if (completed) return <></>
-          return (
-            <div className="flex my-4 md:my-2 lg:my-5 2xl:my-3 3xl:my-4">
-              <div className="text-center">
-                {days > 0 && (
-                  <div className="md:hidden mb-3">
-                    <TimeCard>
-                      {days}
-                      <span className="pl-1">{getDayText(days)}</span>
-                    </TimeCard>
-                  </div>
-                )}
-                <div className="md:text-xl flex items-center">
-                  {days > 0 && (
-                    <div className="hidden md:flex">
-                      <TimeCard>
-                        <span className="px-1 md:px-2">{days}</span>
-                      </TimeCard>
-                      <span className="px-2 inline-flex justify-center items-center text-white">
-                        {getDayText(days)}
-                      </span>
-                    </div>
-                  )}
+    <>
+      {isLoaded && (
+        <Countdown
+          date={date}
+          renderer={({ days, hours, minutes, seconds, completed }) => {
+            // if (completed) return <></>
+            return (
+              <RenderCountDown
+                dayText={getDayText(days)}
+                days={days}
+                hours={hours}
+                minutes={minutes}
+                seconds={seconds}
+              />
+            )
+          }}
+        ></Countdown>
+      )}
 
-                  <TimeCard>{zeroPad(hours)}</TimeCard>
+      {/* 静态的倒计时DOM SSR渲染用 */}
+      {!isLoaded && (
+        <RenderCountDown
+          dayText={getDayText(staticTimeInfo.days)}
+          {...staticTimeInfo}
+        />
+      )}
+    </>
+  )
+}
 
-                  <Semicolon />
-
-                  <TimeCard>{zeroPad(minutes)}</TimeCard>
-
-                  <Semicolon />
-
-                  <TimeCard>{zeroPad(seconds)}</TimeCard>
-                </div>
-              </div>
+function RenderCountDown({
+  days,
+  hours,
+  minutes,
+  seconds,
+  dayText,
+}: {
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+  dayText: string
+}) {
+  return (
+    <div className="flex my-4 md:my-2 lg:my-5 2xl:my-3 3xl:my-4">
+      <div className="text-center">
+        {days > 0 && (
+          <div className="md:hidden mb-3">
+            <TimeCard>
+              {days}
+              <span className="pl-1">{dayText}</span>
+            </TimeCard>
+          </div>
+        )}
+        <div className="md:text-xl flex items-center">
+          {days > 0 && (
+            <div className="hidden md:flex">
+              <TimeCard>
+                <span className="px-1 md:px-2">{days}</span>
+              </TimeCard>
+              <span className="px-2 inline-flex justify-center items-center text-white">
+                {dayText}
+              </span>
             </div>
-          )
-        }}
-      ></Countdown>
-    )
+          )}
+
+          <TimeCard>{zeroPad(hours)}</TimeCard>
+
+          <Semicolon />
+
+          <TimeCard>{zeroPad(minutes)}</TimeCard>
+
+          <Semicolon />
+
+          <TimeCard>{zeroPad(seconds)}</TimeCard>
+        </div>
+      </div>
+    </div>
   )
 }
 
