983d96a45f3a7c0ecbc75167ea6eaddae48aac48
yanyusheng <higeme530@gmail.com>
2024-08-30 16:02:30 +0800

    完善locations的城市及图片

--- apps/gemebuild/src/i18n-pages/compost-recycle/locations-page/en.tsx
+++ apps/gemebuild/src/i18n-pages/compost-recycle/locations-page/en.tsx
@@ -3,86 +3,136 @@ import LocationsCardSection, {
   ILocationsCardSectionProps,
 } from './components/LocationsCardSection'
 
-// 卡片Section 1
-const cardSectionProps: ILocationsCardSectionProps = {
-  title: 'BC Locations',
+// 美国地区
+const usSection: ILocationsCardSectionProps = {
+  title: 'US Cities',
   cardList: [
     {
-      title: 'Kelowna - LIVE',
-      isOpen: true,
-      imageUrl: '/assets/images/compost-recycle/city1.webp',
+      title: 'Austin, Texas',
+      imageUrl: '/assets/images/compost-recycle/citys/Austin_Texas.jpg',
     },
     {
-      title: 'Osoyoos',
-      imageUrl: '/assets/images/compost-recycle/city2.webp',
+      title: 'Cape Cod, Massachusetts',
+      imageUrl:
+        '/assets/images/compost-recycle/citys/Cape_Cod_Massachusetts.jpg',
     },
     {
-      title: 'Pentiction',
-      imageUrl: '/assets/images/compost-recycle/city3.webp',
+      title: 'Maine',
+      imageUrl: '/assets/images/compost-recycle/citys/Maine.jpg',
     },
     {
-      title: 'Whistler',
-      imageUrl: '/assets/images/compost-recycle/city4.webp',
+      title: 'Washington, D.C.',
+      imageUrl: '/assets/images/compost-recycle/citys/Washington.jpg',
+    },
+    {
+      title: 'Renton, Washington',
+      imageUrl: '/assets/images/compost-recycle/citys/Renton_Washington.jpg',
+    },
+    {
+      title: 'Spokane, Washington',
+      imageUrl: '/assets/images/compost-recycle/citys/Spokane_Washington.jpg',
+    },
+    {
+      title: 'Tacoma, Washington',
+      imageUrl: '/assets/images/compost-recycle/citys/Tacoma_Washington.jpg',
+    },
+    {
+      title: 'Vancouver, Washington',
+      imageUrl: '/assets/images/compost-recycle/citys/Vancouver_Washington.jpg',
+    },
+    {
+      title: 'Aurora, Colorado',
+      imageUrl: '/assets/images/compost-recycle/citys/Aurora_Colorado.jpg',
+    },
+    {
+      title: 'Arvada, Colorado',
+      imageUrl: '/assets/images/compost-recycle/citys/Arvada_Colorado.jpg',
+    },
+    {
+      title: 'Boulder, Colorado',
+      imageUrl: '/assets/images/compost-recycle/citys/Boulder_Colorado.jpg',
+    },
+    {
+      title: 'Denver, Colorado',
+      imageUrl: '/assets/images/compost-recycle/citys/Denver_Colorado.jpg',
+    },
+    {
+      title: 'Lakewood, Colorado',
+      imageUrl: '/assets/images/compost-recycle/citys/Lakewood_Colorado.jpg',
+    },
+    {
+      title: 'Longmont, Colorado',
+      imageUrl: '/assets/images/compost-recycle/citys/Longmont_Colorado.jpg',
+    },
+    {
+      title: 'Honolulu, Hawaii',
+      imageUrl: '/assets/images/compost-recycle/citys/Honolulu_Hawaii.jpg',
+    },
+    {
+      title: 'Phoenix, Arizona',
+      imageUrl: '/assets/images/compost-recycle/citys/Phoenix_Arizona.jpg',
+    },
+  ],
+
+  locale: 'en',
+}
+
+// 欧洲地区
+const eurSection: ILocationsCardSectionProps = {
+  title: 'European Region',
+  cardList: [
+    {
+      title: 'Paris',
+      imageUrl: '/assets/images/compost-recycle/citys/Paris.jpg',
+    },
+    {
+      title: 'London',
+      imageUrl: '/assets/images/compost-recycle/citys/London.jpg',
+    },
+    {
+      title: 'Rome',
+      imageUrl: '/assets/images/compost-recycle/citys/Rome.jpg',
+    },
+    {
+      title: 'Barcelona',
+      imageUrl: '/assets/images/compost-recycle/citys/Barcelona.jpg',
+    },
+    {
+      title: 'Berlin',
+      imageUrl: '/assets/images/compost-recycle/citys/Berlin.jpg',
+    },
+    {
+      title: 'Brussels',
+      imageUrl: '/assets/images/compost-recycle/citys/Brussels.jpg',
     },
   ],
   locale: 'en',
 }
 
-// 卡片Section 2
-const cardSectionProps2: ILocationsCardSectionProps = {
-  title: 'US Cities',
+// 加拿大地区
+const caSection: ILocationsCardSectionProps = {
+  title: 'BC Locations',
   cardList: [
     {
-      title: 'Austin, Texas',
-      imageUrl: '/assets/images/compost-recycle/city5.webp',
-    },
-    {
-      title: 'Cape Cod, Massachusetts',
-      imageUrl: '/assets/images/compost-recycle/city6.webp',
-    },
-    {
-      title: 'Pentiction',
-      imageUrl: '/assets/images/compost-recycle/city3.webp',
-    },
-    {
-      title: 'Maine',
-      imageUrl: '/assets/images/compost-recycle/city1.webp',
-    },
-    {
-      title: 'Washington, D.C.',
-      imageUrl: '/assets/images/compost-recycle/city2.webp',
-    },
-    {
-      title: 'Renton, Washington',
-      imageUrl: '/assets/images/compost-recycle/city6.webp',
-    },
-    {
-      title: 'Vancouver, Washington',
-      imageUrl: '/assets/images/compost-recycle/city5.webp',
+      title: 'Kelowna - LIVE',
+      imageUrl: '/assets/images/compost-recycle/citys/Kelowna-LIVE.jpg',
     },
     {
       title: 'Osoyoos',
-      imageUrl: '/assets/images/compost-recycle/city6.webp',
+      imageUrl: '/assets/images/compost-recycle/citys/Osoyoos.jpg',
     },
     {
       title: 'Pentiction',
-      imageUrl: '/assets/images/compost-recycle/city3.webp',
+      imageUrl: '/assets/images/compost-recycle/citys/Pentiction.jpg',
     },
     {
       title: 'Whistler',
-      imageUrl: '/assets/images/compost-recycle/city1.webp',
-    },
-    {
-      title: 'Aurora, Colorado',
-      imageUrl: '/assets/images/compost-recycle/city2.webp',
-    },
-    {
-      title: 'Denver, Colorado',
-      imageUrl: '/assets/images/compost-recycle/city6.webp',
+      imageUrl: '/assets/images/compost-recycle/citys/Whistler.jpg',
     },
   ],
   locale: 'en',
 }
+
 export interface ICompostRecycleLocationsPageProps {
   PrefetchLink?: ILinkComponent
 }
@@ -92,11 +142,9 @@ export function CompostRecycleLocationsPageEn({
 }: ICompostRecycleLocationsPageProps) {
   return (
     <>
-      <LocationsCardSection PrefetchLink={PrefetchLink} {...cardSectionProps} />
-      <LocationsCardSection
-        PrefetchLink={PrefetchLink}
-        {...cardSectionProps2}
-      />
+      <LocationsCardSection PrefetchLink={PrefetchLink} {...usSection} />
+      <LocationsCardSection PrefetchLink={PrefetchLink} {...eurSection} />
+      <LocationsCardSection PrefetchLink={PrefetchLink} {...caSection} />
     </>
   )
 }
