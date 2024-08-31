9c3cf0085cec9e88c74019759cedfcd68cb7f233
y <higeme530@gmail.com>
2024-09-01 01:48:36 +0800

    wording

--- apps/gemebuild/src/i18n-pages/geme-v2/en.tsx
+++ apps/gemebuild/src/i18n-pages/geme-v2/en.tsx
@@ -88,7 +88,7 @@ const shortVideoProps: IShortVideoProps = {
 
 // 图文section2
 const imageTextProps: IShortVideoProps = {
-  title: 'Smaller but Powerful',
+  title: 'Smaller but Stronger',
   description: (
     <>
       <ul className="list-disc ml-4 mt-4">
@@ -96,7 +96,7 @@ const imageTextProps: IShortVideoProps = {
         <li className="mt-4">Faster breakdown</li>
         <li className="mt-4">More efficient aerobic air exchange</li>
         <li className="mt-4">More powerful odor filter system</li>
-        <li className="mt-4">Quieter</li>
+        <li className="mt-4">No need to replace filter</li>
         <li className="mt-4">Fit on countertop and floor</li>
       </ul>
     </>
@@ -326,110 +326,182 @@ const faqProps: IFaqProps = {
   faqs: [
     {
       slug: 'what-is-geme',
-      question: 'What is GEME?',
-      answer: `
-        GEME is your little helper for an easy home!
-        <br />
-        No more smelly, leaking garbage, puffing fruit flies, yard full of
-        mice, maggots in the yard waste bin or throwing away food scraps
-        to rot in the landfill.
-        <br />
-        With GEME, you will be able to turn your waste into nutrient-rich
-        organic compost.
-        <br />
-        Revolutionize the way you dispose of your waste while returning
-        nutrient-rich soil to the earth.
-    `,
+      question: 'How it works?',
+      answer: (
+        <>
+          GEME Composters are all home appliances provide and maintain{' '}
+          <strong>an ideal environment</strong> for GEME's special microbes(GEME
+          Kobold) to break down the bio waste. Terra 2 works in the same
+          principle.
+          <br />
+          <ul>
+            <li>
+              <strong>The machine</strong>: Simulates a compost pile in a
+              container, make sure it will not smell.
+              <br />
+              There are some subsystems in the machine
+              <ul>
+                <li>1. Ventilation </li>
+                <li></li>
+                <li></li>
+              </ul>
+              <img src="/assets/images/how-it-works/how-geme-composter-works.jpg" />
+            </li>
+            <li>The Kobold</li>
+          </ul>
+          No more smelly, leaking garbage, puffing fruit flies, yard full of
+          mice, maggots in the yard waste bin or throwing away food scraps to
+          rot in the landfill.
+          <br />
+          With GEME, you will be able to turn your waste into nutrient-rich
+          organic compost.
+          <br />
+          Revolutionize the way you dispose of your waste while returning
+          nutrient-rich soil to the earth.
+        </>
+      ),
     },
     {
       slug: 'what-s-bio-waste',
-      question: 'What’s bio-waste?',
-      answer: `Bio-waste is defined as biodegradable garden and park waste, food and kitchen waste from households, restaurants, caterers and retail premises, and comparable waste from food processing plants. Because GEME-Kobold is a microorganism from nature, it is capable of breaking down most biological waste. For GEME, you can simply understand that GEME can rapidly handle everything you can eat and your little pet's poop.`,
+      question: 'What is the difference between GEME One?',
+      answer: ``,
     },
     {
-      slug: 'why-is-short-time-composting-possible',
-      question: 'Why is short-time composting possible?',
-      answer: `
-        <img
-          src="/assets/images/faq/why-is-short-time-composting-possible.png"
-          alt="short-time composting is possible"
-        />
-        Unlike previous facilities that call themselves fast composters,
-        GEME replicates the entire process of natural composting,
-        accelerating the decomposition of bio-waste by providing a more
-        suitable environment for the microbiota to multiply and decompose.
-        <br />
-        GEME-Kobold is the world's only proven high-temperature resistant
-        microbial complex from nature, and it is this microbial complex
-        that plays a key role in composting.
-        <br />
-        Therefore, GEME not only achieves short composting times, but
-        because of GEME-Kobold, the compost has high activity and high
-        nutritional value.
-      `,
+      slug: 'what-s-bio-waste',
+      question:
+        'What is the difference between other electric composters like Lomi?',
+      answer: ``,
     },
+    // {
+    //   slug: 'can-I-add-food-waste-when-it-is-running',
+    //   question: 'Can I add food waste when it is running?',
+    //   answer: ``,
+    // },
     {
-      slug: 'can-food-waste-be-composted',
-      question: 'Can food waste be composted?',
-      answer: `Yes, it is possible. Unlike traditional garden composting, GEME was designed for food waste! Food waste is rich in protein and is one of the best sources of input for composting. So let your little GEME help you eat the greasy leftovers and produce organic fertilizer! Tip: Remember to drain the vegetable broth, too wet environment is not conducive to composting, although the GEME can automatically sense dehumidification, but the effort to save a little energy consumption, is not a good thing!`,
+      slug: 'what-s-bio-waste',
+      question: 'What kinds of food waste can I put?',
+      answer: ``,
     },
-    {
-      slug: 'is-it-possible-to-avoid-the-use-of-microbiome',
-      question: 'Is it possible to avoid the use of microbiome?',
-      answer: `
-        The essence of composting is a dynamic process achieved by the
-        interaction of multiple microbial communities with very rapid
-        community structure succession. The initial heating and high
-        temperature stage can play a role in killing harmful substances
-        such as pathogenic pathogens, insect eggs, weed seeds, etc., but
-        the main role of microorganisms in this process also includes
-        metabolism and reproduction, while producing a small amount of
-        metabolites, which are neither stable nor easy to be absorbed by
-        plants; after the cooling and decomposition period in the late
-        stage of composting, microorganisms will carry out the
-        humification of organic matter, and in this process produce a
-        large number of metabolites that are beneficial to plant growth
-        and absorption. Therefore, traditional composting generally takes
-        4-6 months.
-        <br />
-        No microorganism in nature can establish an effective and stable
-        microbial population within 24 hours, and the minimum incubation
-        time is generally 48-72 hours, which refers to the process of
-        adapting microorganisms to the new environment at the beginning of
-        composting, that is, the process of domestication. After the end
-        of the domestication process, it enters the middle temperature
-        growth stage and the high temperature stage, which represents the
-        settlement of microorganisms in the reactor and the initiation of
-        the degradation process. Therefore, if the microbiota is not
-        added, then after drying and dehydrating through the equipment in
-        24 hours or even less, only the charred and dehydrated garbage is
-        obtained, not organic fertilizer. This is also the biggest
-        difference between GEME and other short-term waste drying
-        processors.
-        <br />
-        So the microbiome must be added.
-      `,
-    },
-    {
-      slug: 'can-geme-kobold-regenerate',
-      question: 'Can GEME-Kobold regenerate?',
-      answer: `GEME-Kobold is a highly regenerative microbiota. But don't forget
-    that after rapid composting, the organic fertilizer produced is only
-    5% of the volume of biological waste that was originally put in, and
-    if a large amount of biological waste is continuously poured after
-    this, the effect and speed of decomposition must be affected. So we
-    recommend that you add GEME-Kobold in a very small amount every two
-    weeks, and only 20g can keep your GEME-Kobold permanently active.`,
-    },
+    // {
+    //   slug: 'what-is-the-electricity-cost',
+    //   question:
+    //     'Aside from food waste, can I put other waste like tissue, paper, or bioplastics?',
+    //   answer: ``,
+    // },
     {
       slug: 'how-long-will-it-take-for-geme-to-turn-my-food-waste-into-compost',
-      question:
-        'How long will it take for GEME to turn my food waste into compost?',
-      answer: `It has been experimentally determined that more than 380 kinds of
-    biological waste can be rapidly decomposed in GEME within 6-8 hours.
-    If you break up your waste as much as possible, the increased
-    surface area will result in faster decomposition.`,
+      question: 'How long will it take to turn my food waste into compost?',
+      answer: ``,
     },
+    {
+      slug: 'what-is-the-electricity-cost',
+      question: 'Is it smell? How often should I replace the carbon filter?',
+      answer: ``,
+    },
+
+    {
+      slug: 'what-is-the-electricity-cost',
+      question: 'How often should I clean the bin?',
+      answer: ``,
+    },
+
+    {
+      slug: 'what-is-the-price',
+      question: 'What is the maintenance cost for keep the unit?',
+      answer: ``,
+    },
+
+    {
+      slug: 'what-is-the-price',
+      question: 'When will it be on sale? and what is the price?',
+      answer: ``,
+    },
+
+    //--
+    // {
+    //   slug: 'why-is-short-time-composting-possible',
+    //   question: 'What kinds of food waste can not put',
+    //   answer: `
+    //     <img
+    //       src="/assets/images/faq/why-is-short-time-composting-possible.png"
+    //       alt="short-time composting is possible"
+    //     />
+    //     Unlike previous facilities that call themselves fast composters,
+    //     GEME replicates the entire process of natural composting,
+    //     accelerating the decomposition of bio-waste by providing a more
+    //     suitable environment for the microbiota to multiply and decompose.
+    //     <br />
+    //     GEME-Kobold is the world's only proven high-temperature resistant
+    //     microbial complex from nature, and it is this microbial complex
+    //     that plays a key role in composting.
+    //     <br />
+    //     Therefore, GEME not only achieves short composting times, but
+    //     because of GEME-Kobold, the compost has high activity and high
+    //     nutritional value.
+    //   `,
+    // },
+    // {
+    //   slug: 'can-food-waste-be-composted',
+    //   question: 'Can food waste be composted?',
+    //   answer: `Yes, it is possible. Unlike traditional garden composting, GEME was designed for food waste! Food waste is rich in protein and is one of the best sources of input for composting. So let your little GEME help you eat the greasy leftovers and produce organic fertilizer! Tip: Remember to drain the vegetable broth, too wet environment is not conducive to composting, although the GEME can automatically sense dehumidification, but the effort to save a little energy consumption, is not a good thing!`,
+    // },
+    // {
+    //   slug: 'is-it-possible-to-avoid-the-use-of-microbiome',
+    //   question: 'Is it possible to avoid the use of microbiome?',
+    //   answer: `
+    //     The essence of composting is a dynamic process achieved by the
+    //     interaction of multiple microbial communities with very rapid
+    //     community structure succession. The initial heating and high
+    //     temperature stage can play a role in killing harmful substances
+    //     such as pathogenic pathogens, insect eggs, weed seeds, etc., but
+    //     the main role of microorganisms in this process also includes
+    //     metabolism and reproduction, while producing a small amount of
+    //     metabolites, which are neither stable nor easy to be absorbed by
+    //     plants; after the cooling and decomposition period in the late
+    //     stage of composting, microorganisms will carry out the
+    //     humification of organic matter, and in this process produce a
+    //     large number of metabolites that are beneficial to plant growth
+    //     and absorption. Therefore, traditional composting generally takes
+    //     4-6 months.
+    //     <br />
+    //     No microorganism in nature can establish an effective and stable
+    //     microbial population within 24 hours, and the minimum incubation
+    //     time is generally 48-72 hours, which refers to the process of
+    //     adapting microorganisms to the new environment at the beginning of
+    //     composting, that is, the process of domestication. After the end
+    //     of the domestication process, it enters the middle temperature
+    //     growth stage and the high temperature stage, which represents the
+    //     settlement of microorganisms in the reactor and the initiation of
+    //     the degradation process. Therefore, if the microbiota is not
+    //     added, then after drying and dehydrating through the equipment in
+    //     24 hours or even less, only the charred and dehydrated garbage is
+    //     obtained, not organic fertilizer. This is also the biggest
+    //     difference between GEME and other short-term waste drying
+    //     processors.
+    //     <br />
+    //     So the microbiome must be added.
+    //   `,
+    // },
+    // {
+    //   slug: 'can-geme-kobold-regenerate',
+    //   question: 'Can GEME-Kobold regenerate?',
+    //   answer: `GEME-Kobold is a highly regenerative microbiota. But don't forget
+    // that after rapid composting, the organic fertilizer produced is only
+    // 5% of the volume of biological waste that was originally put in, and
+    // if a large amount of biological waste is continuously poured after
+    // this, the effect and speed of decomposition must be affected. So we
+    // recommend that you add GEME-Kobold in a very small amount every two
+    // weeks, and only 20g can keep your GEME-Kobold permanently active.`,
+    // },
+    // {
+    //   slug: 'how-long-will-it-take-for-geme-to-turn-my-food-waste-into-compost',
+    //   question:
+    //     'How long will it take for GEME to turn my food waste into compost?',
+    //   answer: `It has been experimentally determined that more than 380 kinds of
+    // biological waste can be rapidly decomposed in GEME within 6-8 hours.
+    // If you break up your waste as much as possible, the increased
+    // surface area will result in faster decomposition.`,
+    // },
   ],
 }
 
