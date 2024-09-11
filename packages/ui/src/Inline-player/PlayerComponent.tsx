a2469f4eee1f5ef5cc4d24771777409ea8498242
yanyusheng <higeme530@gmail.com>
2024-09-11 18:22:20 +0800

    fix

--- packages/ui/src/Inline-player/PlayerComponent.tsx
+++ packages/ui/src/Inline-player/PlayerComponent.tsx
@@ -16,7 +16,7 @@ export interface IPlayerComponentType {
   videoIsAutoPlay?: boolean
 }
 
-export function PlayerComponent({
+export default function PlayerComponent({
   videoPosterUrl,
   type,
   videoSrcUrlOrVidioId,
