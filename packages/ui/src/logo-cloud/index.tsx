e940ee1e92d3525db3ceb4b14463bf6ef711ce8a
yanyusheng <higeme530@gmail.com>
2024-09-19 17:12:24 +0800

    新闻媒体logo重构

--- packages/ui/src/logo-cloud/index.tsx
+++ packages/ui/src/logo-cloud/index.tsx
@@ -18,50 +18,61 @@ const titleTextInfo = {
 // Logo cloud
 const logoCloudImages: TItem[] = [
   {
-    src: '/assets/images/geme-terra-2/press/europa-press.png',
+    src: '/assets/images/logo-cloud/aplogo.svg',
+    alt: 'AP',
+    link: 'https://apnews.com/press-release/ein-presswire-newsmatics/recycling-financial-markets-488e391a508177695876215b25369f49',
+  },
+  {
+    src: '/assets/images/logo-cloud/benzinga.png',
+    alt: 'Benzinga',
+    link: 'https://www.benzinga.com/content/40881334/composter-market-chaos-geme-urgent-need-for-industry-standards',
+  },
+  {
+    src: '/assets/images/logo-cloud/europa-press.png',
     alt: 'Europa Press',
     link: 'https://www.europapress.es/comunicados/internacional-00907/noticia-comunicado-revolucionando-sostenibilidad-hogar-geme-presenta-futuro-compostaje-ifa-berlin-2024-20240830060937.html',
   },
   {
-    src: '/assets/images/geme-terra-2/press/adnkronos.svg',
+    src: '/assets/images/logo-cloud/adnkronos.svg',
     alt: 'Adn Kronos',
     link: 'https://www.adnkronos.com/immediapress/rivoluzionare-la-sostenibilita-domestica-geme-presenta-il-futuro-del-compostaggio-alledizione-2024-di-ifa-a-berlino_6MKcoIpt8YP0E5h7SoRmIg',
   },
   {
-    src: '/assets/images/geme-terra-2/press/bolsamania.png',
+    src: '/assets/images/logo-cloud/bolsamania.png',
     alt: 'Bolsamania',
     link: 'https://www.bolsamania.com/nota-de-prensa/mercados/revolucionando-la-sostenibilidad-del-hogar-geme-presenta-el-futuro-del-compostaje-en-ifa-berlin-2024--17375550.html',
   },
+
   {
-    src: '/assets/images/geme-terra-2/press/wallstreet-online-horizontal.svg',
-    alt: 'Reform',
-    link: 'https://www.wallstreet-online.de/nachricht/18438137-geme-revolutioniert-nachhaltigkeit-haushalt-stellt-ifa-berlin-2024-zukunft-kompostierung',
-  },
-  {
-    src: '/assets/images/geme-terra-2/press/yahoo-finance.png',
-    alt: 'Yahoo Finance',
-    link: 'https://consent.yahoo.com/v2/collectConsent?sessionId=4_cc-session_2208ea75-58aa-459e-ac89-0cf35164b34c',
-  },
-  {
-    src: '/assets/images/geme-terra-2/press/advfn.png',
-    alt: 'ADVFN',
-    link: 'https://www.advfn.com/stock-market/stock-news/94437900/revolutionizing-home-sustainability-geme-debuts-th',
-  },
-  {
-    src: '/assets/images/geme-terra-2/press/IFA.svg',
+    src: '/assets/images/logo-cloud/IFA.svg',
     alt: 'IFA',
     link: 'https://daily.ifa-berlin.com/daily/geme-smart-composter/',
   },
   {
-    src: '/assets/images/geme-terra-2/press/housedigest.svg',
+    src: '/assets/images/logo-cloud/housedigest.svg',
     alt: 'Housedigest',
     link: 'https://www.housedigest.com/1660656/ifa-2024-these-home-garden-innovations-offer-a-high-tech-twist/',
   },
   {
-    src: '/assets/images/geme-terra-2/press/digitaltrends.png',
+    src: '/assets/images/logo-cloud/digitaltrends.jpg',
     alt: 'Digitaltrends',
     link: 'https://www.digitaltrends.com/home/ifa-2024-best-smart-home/',
   },
+  {
+    src: '/assets/images/logo-cloud/wallstreet-online-horizontal.svg',
+    alt: 'Reform',
+    link: 'https://www.wallstreet-online.de/nachricht/18438137-geme-revolutioniert-nachhaltigkeit-haushalt-stellt-ifa-berlin-2024-zukunft-kompostierung',
+  },
+  {
+    src: '/assets/images/logo-cloud/yahoo-finance.png',
+    alt: 'Yahoo Finance',
+    link: 'https://consent.yahoo.com/v2/collectConsent?sessionId=4_cc-session_2208ea75-58aa-459e-ac89-0cf35164b34c',
+  },
+  // {
+  //   src: '/assets/images/logo-cloud/advfn.png',
+  //   alt: 'ADVFN',
+  //   link: 'https://www.advfn.com/stock-market/stock-news/94437900/revolutionizing-home-sustainability-geme-debuts-th',
+  // },
 ]
 
 export function LogoCloud({
@@ -102,7 +113,9 @@ export function LogoCloudComponent({
             <a
               href={item.link}
               target="_blank"
-              className={clsx(index === 2 ? 'md:col-span-2' : '')}
+              className={clsx(
+                index === logoCloudImages.length - 1 ? 'hidden md:block' : '',
+              )}
             >
               <ImageComponents item={item} />
             </a>
@@ -110,8 +123,10 @@ export function LogoCloudComponent({
 
           {!canRedirect && (
             <ImageComponents
+              className={clsx(
+                index === logoCloudImages.length - 1 ? 'hidden md:block' : '',
+              )}
               item={item}
-              className={clsx(index === 2 ? 'md:col-span-2' : '')}
             />
           )}
         </Fragment>
